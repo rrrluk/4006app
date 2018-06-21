22703231ca6aa2f11397b32f241de25d1e4be880
Pertti Johanson <rrrluk@gmail.com>
2018-06-21 16:15:51 +0300

    sama seis mis enne, loogika app.js sees

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
  app.set("view engine", "ejs");
  // ei saa DO reverse proxyt tööle muidu, läheb / kataloogist otsima neid views-i
- //  app.set('views', '4006app/views');
+  app.set('views', '4006app/views');
 
  app.get("/4006app", function (req, res) {
      res.render("home");
@@ -19,6 +19,23 @@
      //  console.log(req.body);
      //   console.log(req.body);
      var dataIn = req.body;
+     var macData = "";
+
+     // loobib üle req.body ja paneb kokku stringi maci arvutamise jaoks
+     Object.keys(dataIn).forEach(function (k) {
+         if (k != "VK_MAC") {
+             var item = dataIn[k]
+             if (item.length < 10) {
+                 macData += "00" + item.length + item;
+             } else if (item.length < 100) {
+                 macData += "0" + item.length + item;
+             } else {
+                 macData += item;
+             }
+         };
+     });
+
+     //  console.log(macData);
 
      //  Object.keys(req.body).forEach(function (k) {
      //      if (k != "VK_MAC") {
@@ -27,7 +44,8 @@
      //  });
 
      res.render("received", {
-         dataIn: dataIn
+         dataIn: dataIn,
+         macData: macData,
      });
      //  res.send("incoming!");
 
