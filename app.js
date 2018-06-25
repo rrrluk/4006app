f160ae088061a441eacdbd069aa7eabdcc1404ec
Pertti Johanson <rrrluk@gmail.com>
2018-06-25 14:48:39 +0300

    Verify true sissetulnud sõnumiga

--- app.js
+++ app.js
@@ -3,72 +3,76 @@ var app = express();
 var bodyParser = require("body-parser");
 var fs = require("fs");
 const crypto = require("crypto");
-const verify = crypto.createVerify("RSA-SHA1");
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 
 app.set("view engine", "ejs");
 // ei saa DO reverse proxyt tööle muidu, läheb / kataloogist otsima neid views-i
-//  app.set('views', '4006app/views');
+// app.set('views', '4006app/views');
 
-app.get("/4006app", function(req, res) {
-  res.render("home");
-  //  res.send("Works");
+app.get("/4006app", function (req, res) {
+    res.render("home");
+    //  res.send("Works");
 });
 
-app.post("/4006app/incoming", function(req, res) {
-  //  console.log(req.body);
-  //   console.log(req.body);
-  var dataIn = req.body;
-  var sig = req.body.VK_MAC;
-  var macData = "";
+app.post("/4006app/incoming", function (req, res) {
+    //  console.log(req.body);
+    //   console.log(req.body);
+    var dataIn = req.body;
+    var sig = req.body.VK_MAC;
+    var macData = "";
 
-  // loobib üle req.body ja paneb kokku stringi maci arvutamise jaoks
-  Object.keys(dataIn).forEach(function(k) {
-    if (k != "VK_MAC") {
-      var item = dataIn[k];
-      if (item.length < 10) {
-        macData += "00" + item.length + item;
-      } else if (item.length < 100) {
-        macData += "0" + item.length + item;
-      } else {
-        macData += item.length + item;
-      }
-    }
-  });
+    // loobib üle req.body ja paneb kokku stringi maci arvutamise jaoks
+    Object.keys(dataIn).forEach(function (k) {
+        if (k != "VK_MAC") {
+            var item = dataIn[k];
+            if (item.length < 10) {
+                macData += "00" + item.length + item;
+            } else if (item.length < 100) {
+                macData += "0" + item.length + item;
+            } else {
+                macData += item.length + item;
+            }
+        }
+    });
 
-  var macDataHash = crypto
-    .createHash("sha1")
-    .update(macData)
-    .digest("hex");
 
-  //  console.log(macData);
-  //  console.log(macDataHash);
+    //  console.log(macData);
+    //  console.log(macDataHash);
 
-  // local path
-  var pubKey = fs.readFileSync("test-acs-BL_4006.pub", "utf8");
-  // reverse proxy path
-  //  var pubKey = fs.readFileSync("4006app/test-acs-BL_4006.pub", "utf8");
-  //  console.log(pubKey);
-  verify.update(macData);
-  var sigDecoded = Buffer.from(sig, "base64").toString();
-  console.log(verify.verify(pubKey, sigDecoded));
+    // GET KEY
+    // local path
+    var pubKey = fs.readFileSync("test-acs-BL_4006.pub");
+    // DO reverse proxy path
+    // var pubKey = fs.readFileSync("4006app/test-acs-BL_4006.pub");
 
-  //   console.log(sig);
-  //   console.log(macDataHash);
+    // GET DATA
+    const verify = crypto.createVerify('RSA-SHA1');
+    verify.update(macData);
 
-  res.render("received", {
-    dataIn: dataIn,
-    macData: macData,
-    macDataHash: macDataHash,
-    pubKey: pubKey
-  });
+    // SIGNATURE decode base64
+    var sigDecoded = new Buffer(sig, 'base64');
+
+    // VERIFY
+    console.log(verify.verify(pubKey, sigDecoded));
+
+    //   console.log(sig);
+    //   console.log(macDataHash);
+
+    res.render("received", {
+        dataIn: dataIn,
+        macData: macData,
+        // macDataHash: macDataHash,
+        pubKey: pubKey
+    });
 });
 
-app.listen(8004, "localhost", function() {
-  console.log("4006app started!");
+
+// SERVER START
+app.listen(8004, "localhost", function () {
+    console.log("4006app started on port 8004!");
 });
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 appName=4006app
+host=167.99.83.62
 
 tar czf $appName.tar.gz *.js *.json views *.pub *.key
-tsocks scp $appName.tar.gz pertti@188.166.168.161:/home/pertti/
+tsocks scp $appName.tar.gz pertti@$host:~
 rm $appName.tar.gz
 # remote
-tsocks ssh pertti@188.166.168.161 <<'ENDSSH'
+tsocks ssh pertti@$host<<'ENDSSH'
 # variable uuesti, remote jaoks
 appName=4006app
 rm -rf $appName
