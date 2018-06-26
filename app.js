a365d9012259d336f5ff73f7a7c29cce9a778457
Pertti Johanson <rrrluk@gmail.com>
2018-06-26 13:13:32 +0300

    Form valmis, alustan tagasimineva post routega

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 var bodyParser = require("body-parser");
 var fs = require("fs");
 const crypto = require("crypto");
+var moment = require('moment');
+
+// Minu variabled
+var returnUrl = "";
 
 app.use(
     bodyParser.urlencoded({
@@ -12,7 +16,7 @@ app.use(
 
 app.set("view engine", "ejs");
 // ei saa DO reverse proxyt tööle muidu, läheb / kataloogist otsima neid views-i
-// app.set('views', '4006app/views');
+app.set('views', '4006app/views');
 
 app.get("/4006app", function (req, res) {
     res.render("home");
@@ -24,6 +28,9 @@ app.post("/4006app/incoming", function (req, res) {
     //   console.log(req.body);
     var dataIn = req.body;
     var sig = req.body.VK_MAC;
+    returnUrl = req.body.VK_RETURN;
+    var vk_nonce = req.body.VK_NONCE;
+    var vk_userId = req.body.VK_USER;
     var macData = "";
 
     // loobib üle req.body ja paneb kokku stringi maci arvutamise jaoks
@@ -40,35 +47,49 @@ app.post("/4006app/incoming", function (req, res) {
         }
     });
 
-
-    //  console.log(macData);
-    //  console.log(macDataHash);
-
     // GET KEY
     // local path
-    var pubKey = fs.readFileSync("test-acs-BL_4006.pub");
+    // var pubKey = fs.readFileSync("test-acs-BL_4006.pub");
     // DO reverse proxy path
-    // var pubKey = fs.readFileSync("4006app/test-acs-BL_4006.pub");
+    var pubKey = fs.readFileSync("4006app/test-acs-BL_4006.pub");
 
-    // GET DATA
+    // INIT verfiy
     const verify = crypto.createVerify('RSA-SHA1');
+    // GET DATA to verify
     verify.update(macData);
 
     // SIGNATURE decode base64
     var sigDecoded = new Buffer(sig, 'base64');
 
     // VERIFY
-    console.log(verify.verify(pubKey, sigDecoded));
+    // console.log(verify.verify(pubKey, sigDecoded));
+    var vResult = verify.verify(pubKey, sigDecoded);
 
-    //   console.log(sig);
-    //   console.log(macDataHash);
+    Object.keys(dataIn).forEach(function (k) {
+        console.log(k + " - " + dataIn[k]);
+    });
+
+    var date = moment().format("YYYYMMDD");
+    var time = moment().format("HH:mm:ss");
+
 
     res.render("received", {
         dataIn: dataIn,
+        returnUrl: returnUrl,
+        vk_nonce: vk_nonce,
+        date: date,
+        time: time,
+        vk_userId: vk_userId,
         macData: macData,
-        // macDataHash: macDataHash,
-        pubKey: pubKey
+        pubKey: pubKey,
+        vResult: vResult,
     });
+});
+
+app.post("/4006app/outgoing", function (req, res) {
+    console.log(returnUrl);
+    // res.redirect(returnUrl);
+
 });
 
 
